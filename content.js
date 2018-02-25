0042471c51e8932626eaf0408dbeabf04889fecb
Arjit Gupta <arjit.98g@gmail.com>
2018-02-25 16:11:08 +0530

    added files
    Js files for extension

---
+++ content.js
@@ -0,0 +1,11 @@
+//content.js
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
